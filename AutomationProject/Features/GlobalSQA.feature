c6aa319fd64f3b9b87b1cf9a4012f4e8ad9da1ee
Deo Prasad Shaw <deoshaw3@gmail.com>
2021-10-22 11:31:31 +0530

    Added drag and drop feature

---
+++ AutomationProject/Features/GlobalSQA.feature
@@ -0,0 +1,8 @@
+Feature: GlobalSQA
+Background: GlobalSQA Login
+Given GlobalSQA portal is open
+
+@sqa
+Scenario: Drag the Element around
+When the element is visible on the webpage
+Then User drags it around
--- AutomationProject/src/test/java/hook/Hook.java
+++ AutomationProject/src/test/java/hook/Hook.java
@@ -2,7 +2,10 @@ package hook;
 
 import java.util.concurrent.TimeUnit;
 
-
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -10,26 +13,27 @@ import stepDefinition.BaseStepDefinition;
 
 //public class Hook extends BaseStepDefinition {
 public class Hook {
-	
+	WebDriver driver;
+	WebDriverWait wait;
+	JavascriptExecutor js;
 	
 	@Before(order = 0)
     public void launchBrowser(){
 		
-		BaseStepDefinition.LaunchBrowser();
-		/*System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bishal\\chromedriver.exe");
+		   BaseStepDefinition.LaunchBrowser();
+		   /*System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bishal\\chromedriver.exe");
 		   driver=new ChromeDriver();
 		   driver.manage().window().maximize();
 		   driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		   wait=new WebDriverWait(driver,10);
-		   js=(JavascriptExecutor)driver;*/
-
-		   System.out.println("Browser launched");
+		   js=(JavascriptExecutor)driver;
+           System.out.println("Browser launched");*/
     } 
  
- @After(order = 0)
+    @After(order = 0)
     public void tearDown(){
-	 
-	 BaseStepDefinition.CloseBrowser();
+    	//driver.quit();
+	   BaseStepDefinition.CloseBrowser();
        
     } 
 
