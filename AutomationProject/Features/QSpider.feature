9b336ffff722f96fdc4511c5481b11fe1fbbd0a0
Deo Prasad Shaw <deoshaw3@gmail.com>
2024-09-28 09:47:08 +0530

    Added QSpider new window scenario

--- AutomationProject/Features/QSpider.feature
+++ AutomationProject/Features/QSpider.feature
@@ -1,7 +1,15 @@
 Feature: Qspider
-@qspider
-Scenario: Validate modal
+Background: Open QSpider Portal
 Given QSpider portal is open
+
+@qspidermodal
+Scenario: Validate modal
 When user navigates to modal 
 And user clicks on open modal button
 Then modal pop up is displayed
+
+@qspiderwindow
+Scenario: Navigate to new login window
+When User navigates to new login window
+And User switches to the new login window
+Then User is on new login window
--- AutomationProject/src/test/java/stepDefinition/QSpiderStepDefinition.java
+++ AutomationProject/src/test/java/stepDefinition/QSpiderStepDefinition.java
@@ -1,6 +1,9 @@
 package stepDefinition;
 
+import java.util.Iterator;
+import java.util.Set;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 
 import io.cucumber.java.en.Given;
@@ -35,6 +38,36 @@ public class QSpiderStepDefinition extends BaseStepDefinition
 	    Thread.sleep(2000);
 	    driver.findElement(By.cssSelector("button#closeModal")).click();
 	    Thread.sleep(1000);
+	}
+	
+	@When("User navigates to new login window")
+	public void user_navigates_to_new_login_window() {
+		 driver.findElement(By.xpath("(//div[text()='Explore more'])[1]")).click();
+		 driver.findElement(By.xpath("//section[text()='Popups']/..")).click();
+		 driver.findElement(By.xpath("//a[@href='/ui/browser']")).click();
+		 driver.findElement(By.cssSelector("a#browserLink1")).click();
+	}
+	@When("User switches to the new login window")
+	public void user_switches_to_the_new_login_window() {
+	    
+		String parentWindow=driver.getWindowHandle();
+		Set<String> allWindows=driver.getWindowHandles();
+		Iterator<String> itr=allWindows.iterator();
+		String currentWindow;
+		while(itr.hasNext())
+		{
+			currentWindow=itr.next();
+			if(!currentWindow.equalsIgnoreCase(parentWindow))
+			{
+				driver.switchTo().window(currentWindow);
+			}
+		}
+	}
+	@Then("User is on new login window")
+	public void user_is_on_new_login_window() 
+	{
+	   wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='Sign Up']")));
+	   Assert.assertTrue(driver.getCurrentUrl().contains("/ui/browser/"));
 	}
 
 
