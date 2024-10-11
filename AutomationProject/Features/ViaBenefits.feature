e36a4c2f3a1f20bcdfe475e79820044b21e11a29
Deo Prasad Shaw <deoshaw3@gmail.com>
2024-10-11 17:36:55 +0530

    Added ViaBenefits login scenario

--- AutomationProject/Features/ViaBenefits.feature
+++ AutomationProject/Features/ViaBenefits.feature
@@ -1,7 +1,12 @@
 Feature: ViaBenefits
-@viabenefits
-Scenario: Navigate to login Page of spending accounts
+
+Background: Navigate to login Page of spending accounts
 Given Via Benefits portal is open
 When User navigates to login page of Via Benefits
 Then Login page of Via Benefits is loaded with below message
 |Welcome to Via Benefits|Sign into your Account|
+
+@viabenefits
+Scenario: Login with wrong credentials
+When User logs in with wrong credentials
+Then error message is displayed on the ViaBenefits portal
--- AutomationProject/src/test/java/stepDefinition/ViaBenefitsStepDefinition.java
+++ AutomationProject/src/test/java/stepDefinition/ViaBenefitsStepDefinition.java
@@ -3,6 +3,10 @@ package stepDefinition;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 
 import io.cucumber.java.en.Given;
@@ -18,7 +22,8 @@ public class ViaBenefitsStepDefinition extends BaseStepDefinition
 	    
 	}
 	@When("User navigates to login page of Via Benefits")
-	public void user_navigates_to_login_page_of_via_benefits() {
+	public void user_navigates_to_login_page_of_via_benefits() 
+	{
 	    driver.findElement(By.xpath("(//a[text()='Sign In'])[1]")).click();
 	}
 	@Then("Login page of Via Benefits is loaded with below message")
@@ -29,6 +34,25 @@ public class ViaBenefitsStepDefinition extends BaseStepDefinition
 		String textMessage=driver.findElement(By.xpath("//*[text()='"+text+"']")).getText();
 		Assert.assertEquals(textMessage, text);
 	    
+	}
+	
+	@When("User logs in with wrong credentials")
+	public void user_logs_in_with_wrong_credentials() 
+	{
+		driver.findElement(By.cssSelector("input#username")).sendKeys("Test123@gmail.com");
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		WebElement passwordInputBox=driver.findElement(By.cssSelector("input#passwordInput"));
+		wait.until(ExpectedConditions.visibilityOf(passwordInputBox));
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("arguments[0].value=arguments[1]",passwordInputBox,"test1234");
+		passwordInputBox.sendKeys("test123");
+		passwordInputBox.sendKeys(Keys.ENTER);
+	    
+	}
+	@Then("error message is displayed on the ViaBenefits portal")
+	public void error_message_is_displayed_on_the_via_benefits_portal() 
+	{
+	    Assert.assertEquals(driver.findElement(By.cssSelector("p.err-message:nth-child(2)")).getText(), "Invalid User Credentials");
 	}
 
 
