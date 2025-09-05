f500f5f81df55128f20c60be921d7f43f938e956
Deo Prasad Shaw <deoshaw3@gmail.com>
2025-09-05 08:39:11 +0530

    Added worksoft feature

--- AutomationProject/src/test/java/Runner/TestRunner.java
+++ AutomationProject/src/test/java/Runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
- features = "Features",tags= "@sb",
+ features = "Features",tags= "@worksoft",
  glue={"stepDefinition","hook"},
  dryRun=false,
  monochrome=true,
