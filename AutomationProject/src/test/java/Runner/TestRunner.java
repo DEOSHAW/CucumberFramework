5d8223782b713bd6cb8528c88de33feb5c1a9bb3
Deo Prasad Shaw <deoshaw3@gmail.com>
2025-09-05 08:40:37 +0530

    Revert "Added worksoft feature"
    This reverts commit f500f5f81df55128f20c60be921d7f43f938e956.

--- AutomationProject/src/test/java/Runner/TestRunner.java
+++ AutomationProject/src/test/java/Runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
- features = "Features",tags= "@worksoft",
+ features = "Features",tags= "@sb",
  glue={"stepDefinition","hook"},
  dryRun=false,
  monochrome=true,
